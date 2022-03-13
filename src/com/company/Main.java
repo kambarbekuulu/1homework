da3279c3c90ab15c51b33d389e45b262930555fb
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-03-13 21:13:27 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Azamat");
+        System.out.println("         top oinop jatat  ");
+        System.out.println("                          dostoru menen");
+    }
+}
